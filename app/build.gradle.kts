8658b72071406d16016a7e3c6c03cc1df7fb1472
laminemessaci <lamine.messaci18@gmail.com>
2023-12-17 16:50:44 +0100

    APPBAR added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,11 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+
+    }
 }
 
 dependencies {
@@ -64,6 +69,16 @@ dependencies {
     implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.appcompat:appcompat:1.6.1")
+
+
+    implementation("com.google.android.material:material:1.11.0")
+    implementation ("com.google.dagger:hilt-android:2.40.5")
+
+
+
+
+
+    implementation("androidx.core:core:1.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/lamine/quiz/MainActivity.kt
+++ app/src/main/java/com/lamine/quiz/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.lamine.quiz
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -11,14 +13,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.lamine.quiz.databinding.ActivityMainBinding
 import com.lamine.quiz.ui.theme.QuizTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity()  {
+
+    private lateinit var binding: ActivityMainBinding
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // setContentView(R.layout.activity_main)
+
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
 
         setContent {
             QuizTheme {
@@ -27,11 +41,31 @@ class MainActivity : ComponentActivity()  {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                   // Greeting("Android")
                 }
             }
         }
+        setContentView(view)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                // Action lorsque "Settings" est sélectionné
+                true
+            }
+            R.id.action_about -> {
+                // Action lorsque "About" est sélectionné
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
 
